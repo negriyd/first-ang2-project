1ddddafd82254d39080cc85e7b54b6494ee483fe
negriyd <negriyd@gmail.com>
2016-09-21 18:29:16 +0300

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  private clickCount = 0;
+  private clickEvent;
+
+  getMessage() {
+    return 'Button clicked ' + this.clickCount + ' times';
+  }
+
+  clicked(event) {
+    this.clickCount = this.clickCount + 1;
+    this.clickEvent = event;
+    console.log(event);
+  }
+
+  isCtrlPressed() {
+    return this.clickEvent != null ? this.clickEvent.ctrlKey : 'not clicked';
+  }
 }
